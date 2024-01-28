39d94d08e738fb2abfcf25a6a0cefa455e801292
ItzMarcu <andreamrccc@gmail.com>
2024-01-28 14:10:17 +0100

    Aggiunta della DIR con lo static web

---
+++ script.js
@@ -0,0 +1,79 @@
+const timerElement = document.getElementById('timer');
+const startButton = document.getElementById('tomato-button');
+const navH1 = document.getElementById('title');
+const pText = document.getElementById('p-text');
+
+let isPomodoro = true;
+let isRunning = false;
+let minutes;
+let seconds;
+let timerInterval;
+
+function updateTimer() {
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+    timerElement.textContent = `${formattedMinutes}:${formattedSeconds}`;
+
+    if (minutes === 0 && seconds === 0) {
+        clearInterval(timerInterval);
+        isRunning = false;
+
+        if (isPomodoro) {
+            // Se è il timer Pomodoro, avvia il timer di pausa di 10 minuti
+            startPauseTimer();
+        } else {
+            // Se è il timer di pausa, avvia un nuovo timer Pomodoro di 25 minuti
+            startPomodoroTimer();
+        }
+    } else {
+        if (seconds === 0) {
+            minutes--;
+            seconds = 59;
+        } else {
+            seconds--;
+        }
+    }
+}
+
+function startPomodoroTimer() {
+    isPomodoro = true;
+    minutes = 25;
+    seconds = 0;
+    isRunning = true;
+    timerInterval = setInterval(updateTimer, 1000);
+    document.body.classList.remove('timer-ended');
+    navH1.classList.remove('h1-ended');
+    timerElement.classList.remove('h1-ended');
+    pText.classList.remove('p-ended');
+    pText.innerText = 'Concentrati ora!';
+    updateTimer();
+}
+
+function startPauseTimer() {
+    isPomodoro = false;
+    minutes = 10;
+    seconds = 0;
+    isRunning = true;
+    timerInterval = setInterval(updateTimer, 1000);
+    document.body.classList.add('timer-ended');
+    navH1.classList.add('h1-ended');
+    timerElement.classList.add('h1-ended');
+    pText.classList.add('p-ended');
+    pText.innerText = 'Riposati ora!';
+    updateTimer();
+}
+
+function startTimer() {
+    if (!isRunning) {
+        startPomodoroTimer();
+    }
+}
+
+function resetTimer() {
+    clearInterval(timerInterval);
+    isPomodoro = true;
+    startPomodoroTimer();
+}
+
+startButton.addEventListener('click', startTimer);
+
